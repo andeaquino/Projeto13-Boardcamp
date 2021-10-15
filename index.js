577dab836193d0ad47a22baeac19f7b9204ba261
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-10-15 09:42:56 -0300

    created functions to get and create categories from data bank wvalidating user  input

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import pg from 'pg';
+import Joi from 'joi';
 
 const { Pool } = pg;
 
@@ -11,7 +12,39 @@ const connection = new Pool({
   password: 'senha_super_hiper_ultra_secreta_do_role_do_bootcamp'
 });
 
+const categorySchema = Joi.object({name: Joi.string().required()})
+
 const app = express();
 app.use(express.json());
+
+app.get('/categories', async (req, res) => {
+  try {
+    const categories = await connection.query('SELECT * FROM categories');
+    res.send(categories.rows).status(200);
+  } catch {
+    res.sendStatus(500);
+  }
+});
+
+app.post('/categories', async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const categoryCheck = await connection.query('SELECT * FROM categories WHERE name = $1', [name]);
+
+    if (categoryCheck.rows.length !== 0) {
+      return res.sendStatus(409);
+    }
+  
+    if (categorySchema.validate({name}).error) {
+      return res.sendStatus(400);
+    }
+
+    await connection.query('INSERT INTO categories (name) VALUES ($1)', [name]);
+    res.sendStatus(201);
+  } catch {
+    res.sendStatus(500);
+  }
+});
 
 app.listen(4000);
